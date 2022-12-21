f5440d899375b346244534c1589ec72967904435
Adrien Dubois <adrien.dubois@ens-lyon.fr>
2022-12-21 21:35:21 +0100

    Régression logistion et clustering sur les commentaires des 5 vidéos

--- src/custom_tools.py
+++ src/custom_tools.py
@@ -6,6 +6,6 @@ def eff(y, y_pred):
 
 def flip(e, y_pred):
     if e>0.5:
-        e, y_pred
+        return e, y_pred
     else:
         return 1-e, [1-b for b in y_pred]
--- src/train.py
+++ src/train.py
@@ -1,18 +1,33 @@
 from sklearn.cluster import KMeans
 from lecture_csv import *
-
-csvfiles = bases_noms
-
-X, words, y = extraction_mots(csvfiles)
-kmeans = KMeans(n_clusters=2, random_state=0, n_init="auto").fit(X)
-y_pred = kmeans.labels_
-
-# Mesurons l'efficacité de la prédiction et on inverse les 0 et les 1 si besoin : les clusters peuvent être inversés
 from custom_tools import eff, flip
 
-e = eff(y, y_pred) # Valeur entre 0 si y et y_pred sont inversés et 1 si sont les mêmes
-e, y_pred = flip(y, y_pred)
-print("Efficacité :", e)
+X_full, words, y_full = extraction_mots(bases_noms) # bases_noms contient 5 sets de commentaires
+# X,y jeu d'entrainement et X_test,y_test jeu de validation
+# On procède ainsi pour que d le nombre de mots existants ne varie pas entre le jeu d'entraînement (4 premières vidéos) et de validation (dernière vidéo)
+n = len(extraction_mots(bases_noms[:4])[0])
+X, X_test, y, y_test = X_full[:n], X_full[n:], y_full[:n], y_full[n:] 
+print(f"{len(X)} commentaires sur le jeu d'entraînement et {len(X_test)} sur le jeu de validation")
+
+# Classifieur 1 : KMeans algo de clustering (non supervisé)
+
+kmeans = KMeans(n_clusters=2, random_state=0, n_init="auto").fit(X)
+y_pred1 = kmeans.labels_
+e = eff(y, y_pred1)
+e, y_pred1 = flip(e, y_pred1) # si besoin les clusters peuvent être inversés, du fait de l'apprentissage non supervisé les 0 et 1 peuvent être inversés
+print("\nEfficacité clustering (non supervisé) :", e)
+
+# Classifieur 2 : Régression Logistique (supervisé)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+logistic_regression = LogisticRegression().fit(X, y)
+y_test_pred = logistic_regression.predict(X_test)
+print("\nEfficacité régression logistique sur le modèle d'entraînement (supervisé): ", logistic_regression.score(X, y))
+print("Efficacité régression logistique sur le modèle de validation (supervisé): ", eff(y_test,y_test_pred))
+
+
     
 
 
