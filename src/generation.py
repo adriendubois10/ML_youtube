46ad1fec82bcb0505b7ffdeee3f427d611fd1676
Adrien Dubois <adrien.dubois@ens-lyon.fr>
2023-01-13 21:44:03 +0100

    Choix aléatoire si commentaires limités

--- src/generation.py
+++ src/generation.py
@@ -2,21 +2,21 @@ import openai
 from lecture_csv import *
 from custom_tools import list_to_prompt
 
-openai.api_key = "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"
+openai.api_key = "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" # Attention, nombre limité d'appel
 comments0, comments1 = extraction_comments_label([bases_noms[0]]) # Séparation des commentaires de la vidéo 1 (Psy) selon les étiquettes
 
 gen_comment0 = openai.Completion.create(
     engine="text-davinci-002",
-    prompt=list_to_prompt(comments0, lim=120), # On se limite à 150 lignes
-    max_tokens=30,
-    n=1, # Générer un seul commentaire
-    stop=None, # Pas de condition d'arrêt particulière
-    temperature=0.8, # Sorte de taux de créativité, à quel point le nouveau commentaire s'écarte du modèle
+    prompt=list_to_prompt(comments0, lim=100),
+    max_tokens=30, # Majoration du nombre de mots
+    n=1, 
+    stop=None, # Pas de condition d'arrêt particulière (la condition "\n" ne produit pas de résultat)
+    temperature=0.8, # Sorte de taux d'aléatoire, à quel point le nouveau commentaire s'écarte du modèle
 )
 
 gen_comment1 = openai.Completion.create(
     engine="text-davinci-002",
-    prompt=list_to_prompt(comments1, lim=120),
+    prompt=list_to_prompt(comments1, lim=100),
     max_tokens=50,
     n=1, 
     stop=None,
